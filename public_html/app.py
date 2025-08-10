414e45270fb25c066d1ae9ff8b3d73668998e406
Parvpaigwar <parvpaigwar@gmail.com>
2025-08-10 11:00:58 +0530

    Update app.py

--- public_html/app.py
+++ public_html/app.py
@@ -1,10 +1,7 @@
-import os
-import sys
+import sys, os
 
-# Add current dir to Python path
+# Django settings ka path set karo
 sys.path.insert(0, os.path.dirname(__file__))
-
-# Set default Django settings
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nullnix.settings")
 
 from django.core.wsgi import get_wsgi_application
